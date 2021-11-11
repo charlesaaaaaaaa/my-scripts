64a6fac93d864b93e3b105f0c3cb4a0839bb080a
charlesaaaaaaaa <i1925725098@outlook.com>
2021-11-11 01:15:00 -0500

    bak

--- Kunlun-Qingcloud/script/base/computing-base.sh
+++ Kunlun-Qingcloud/script/base/computing-base.sh
@@ -1,12 +1,12 @@
 source /etc/profile
 
-cd python2 /home/ubuntu/kunlun/kunlun-computing/scripts
+cd /home/ubuntu/kunlun/kunlun-computing/scripts
 
 comp_path=`ps -ef |  grep 'postgres -D' | grep home | awk '{print $10}'`
 
 if [ $1 = start ] ; then
 
-	su ubuntu -c "python2 /home/ubuntu/kunlun/kunlun-computing/scripts/start_pg.py port = 5401"
+	su ubuntu -c "python2 /home/ubuntu/kunlun/kunlun-computing/scripts/start_pg.py port=5401"
 
 elif [ $1 = stop ] ; then
 	su ubuntu -c "/home/ubuntu/kunlun/kunlun-computing/bin/pg_ctl stop -D ${comp_path}"
@@ -16,6 +16,6 @@ elif [ $1 = restart ] ; then
 
 elif [ $1 = destory ] ; then
         su ubuntu -c "/home/ubuntu/kunlun/kunlun-computing/bin/pg_ctl kill 5401"
-else ;
+else 
 	echo please add parameter 'start' 'restart' 'stop' 'destory' 'upgrade' !
 fi
