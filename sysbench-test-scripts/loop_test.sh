fbc67ad39d891c6a6768662e8c4d2fee30788e1c
charlesaaaaaaaa <i1925725098@outlook.com>
2021-12-07 14:05:45 +0800

    bak

--- sysbench-test-scripts/loop_test.sh
+++ sysbench-test-scripts/loop_test.sh
@@ -7,6 +7,20 @@ if [ $# -lt 7 ] ; then
 	exit 1
 fi
 
+
+
+sysbench oltp_point_select        \
+      --tables=$5                   \
+      --table-size=$6           \
+      --db-driver=pgsql             \
+      --pgsql-host=$1        \
+      --pgsql-port=$2             \
+      --pgsql-user=$4         \
+      --pgsql-password=abc \
+      --pgsql-db=$3           \
+      prepare
+
+
 for i in point_select delete insert read_only read_write write_only update_index update_non_index
 do
 	# create test resault dir
@@ -15,7 +29,7 @@ do
 	fi
 done
 
-for i in `seq 1 10` # run sysbench test 100 to 1000 threads
+for i in `seq 4 10` # run sysbench test 100 to 1000 threads
 do
 	li=` expr ${i} \* 100 `
 	echo
@@ -25,5 +39,17 @@ do
 	# like : ./loop_test.sh 192.168.0.134 38701 postgres abc  10        100000     120
 	#        ./loop_test.sh host          port  dbname   user table_num table_size test_time
 done
+
+sysbench oltp_delete        \
+      --tables=$5                   \
+      --table-size=$6           \
+      --db-driver=pgsql             \
+      --pgsql-host=$1        \
+      --pgsql-port=$2             \
+      --pgsql-user=$4         \
+      --pgsql-password=abc \
+      --pgsql-db=$3           \
+      cleanup
+
 
 bash ./resault.sh
--- sysbench-test-scripts/resault.sh
+++ sysbench-test-scripts/resault.sh
@@ -1,11 +1,11 @@
 #
-echo `date` > resault
-for list in point_select delete insert read_only read_write write_only update_index update_non_index
+echo > resault
+for list in point_select insert read_only read_write write_only update_index update_non_index
 do	
-	echo "== ${list} == "	
-	for a in `seq 1 $i` 
+	echo "== ${list} == " >> resault	
+	for a in `seq 1 10` 
 	do
-		$la=`expr $a \* 100`
+		la=`expr $a \* 100`
 		rea=`cat ${list}/${la}_${list} | grep read: | awk '{print $2}'`
 		wri=`cat ${list}/${la}_${list} | grep write: | awk '{print $2}'`
 		txn=`cat ${list}/${la}_${list} | grep transactions: |  awk '{print $2}'`
@@ -14,10 +14,23 @@ do
 		toe=`cat ${list}/${la}_${list} | grep 'events (avg/stddev):' | awk '{print $3}'`
 		echo $la $rea $wri $txn $avg $thp $toe >> resault
 	done
-	echo
+	echo >> resault
 done
 
 sed -i 's/ / || /g' resault
-sed -i 's/^/|| kunlun || /' resault
+sed -i 's/^/|| yugabyte || /' resault
 sed -i 's/$/ ||/' resault
-sed -i '/^/i\|| db || threads || read || wirte || txn || total || avg response time(ms) || .95 response time(ms) || total events ||' resault
+#sed -i '1 i\|| db || threads || read || wirte || txn || total || avg response time(ms) || .95 response time(ms) || total events ||' resault
+sed -i "1 i * `date`" resault
+sed -i "1 i [[PageOutline]]" resault
+
+for i in `seq 0 6`
+do
+        ei=`expr ${i} \* 13 + 4`
+	ai=`expr ${i} \* 13 + 5 `
+	sed -i "$ai i\|| db || threads || read || wirte || txn || avg response time(ms) || .95 response time(ms) || total events ||" resault
+        sed -i "$ei s/||//g" resault
+	sed -i "$ei s/yugabyte//g" resault
+done
+cat resault	
+
