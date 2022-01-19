81fa85983a18c02deae71752af03c299411bf553
charlesaaaaaaaa <i1925725098@outlook.com>
2022-01-19 16:41:26 +0800

    bak

--- bak/ddl-replication.py
+++ bak/ddl-replication.py
@@ -41,6 +41,7 @@ def readJsonFile():
     #print('ip=[' + sip + ']')
     #print('port=' + sport)
     return(ip, port, sip, sport)
+    
 
 def createDbAndConnect():
     num = 0
@@ -108,16 +109,124 @@ def checkAllSystemTable():
         num += 1
 
     
-    for i in ip: #check tmp file
+    for a in range(4): #check tmp file
         num = 0
-        for a in range(3):
-            tbName = SysTabName[a]
-            diff = filecmp.cmp("./ddl-diff/%s%d" %(tbName, num), "./ddl-diff/%s%d" %(tbName, num + 1))
-            if diff :
-                print("./ddl-diff/%s%d" %(tbName, num) + 'is the same as' + "./ddl-diff/%s%d" %(tbName, num + 1) , success)
+        tbName = sysTabName[a]
+        print("\n======== " + tbName + " ========\n")
+        for i in Cns:
+
+            if i == Cns[-1]:
+                pass
             else:
-                print("./ddl-diff/%s%d" %(tbName, num) + 'is not the same as' + "./ddl-diff/%s%d" %(tbName, num + 1) , failure)
-            
+                diff = filecmp.cmp("./ddl-diff/%s%d" %(tbName, num), "./ddl-diff/%s%d" %(tbName, num + 1))
+                if diff :
+                    print(Cns[num] + 'is the same ' + tbName + ' as ' + Cns[num + 1] + " , success")
+                else:
+                    print(Cns[num] + 'is not the same ' + tbName + ' as ' + Cns[num + 1] + " , failure")
+            num += 1
+
+    num = 0
+    dbnum = 1
+    for i in ip:
+        conn=psycopg2.connect(database = 'postgres', user = user[num], host = i, port = port[num], password = pwd[num])
+        conn.autocommit = True
+        cur=conn.cursor()
+        execStmt1 = "select datname  ,encoding ,datcollate, datctype  ,datistemplate ,datallowconn ,datconnlimit ,datlastsysoid ,datfrozenxid ,datminmxid ,dattablespace,datacl from pg_database"
+        cur.execute(execStmt1)
+        res1 = str(cur.fetchall())
+        Res1 = res1.replace(')',')\n')
+        cur.close()
+        conn.close()
+
+        conn=psycopg2.connect(database = 'postgres', user = user[num], host = i, port = port[num], password = pwd[num])
+        conn.autocommit = True
+        cur=conn.cursor()
+        execStmt2 = "select datdba from pg_database where datname = 't%d'" % (dbnum)
+        cur.execute(execStmt2)
+        res2 = str(cur.fetchall())
+        Res2 = res2.replace(')',')\n')
+        cur.close()
+        conn.close()
+
+        res = "%s \n %s" % (Res2, Res1)
+        with open("./ddl-diff/pg_database%d" % (num),"w") as f:
+            f.write(res)
+
+        num += 1
+        dbnum += 1
+
+    print("\n======== pg_database ========\n")
+    num = 0
+    for i in Cns:
+        if i == Cns[-1]:
+            pass
+        else:
+            diff = filecmp.cmp("./ddl-diff/pg_database%d" %(num), "./ddl-diff/pg_database%d" %(num + 1))
+            if diff :
+                print(Cns[num] + 'is the same pg_database as ' + Cns[num + 1] + " , success")
+            else:
+                print(Cns[num] + 'is not the same pg_database as ' + Cns[num + 1] + " , failure")
+        num += 1
+
+
+    num = 0
+    for i in ip:
+        conn=psycopg2.connect(database = 'postgres', user = user[num], host = i, port = port[num], password = pwd[num])
+        conn.autocommit = True
+        cur=conn.cursor()
+        execStmt1 = "select name, id, master_node_id, num_nodes, space_volumn, num_tablets ,db_cluster_id from pg_shard"
+        cur.execute(execStmt1)
+        res = str(cur.fetchall())
+        Res = res1.replace(')',')\n')
+        cur.close()
+        conn.close()
+
+        with open("./ddl-diff/pg_shard%d" % (num),"w") as f:
+            f.write(Res)
+        num += 1
+
+    print("\n======== pg_shard_node ========\n")
+    num = 0
+    for i in Cns:
+        if i == Cns[-1]:
+            pass
+        else:
+            diff = filecmp.cmp("./ddl-diff/pg_shard%d" %(num), "./ddl-diff/pg_shard%d" %(num + 1))
+            if diff :
+                print(Cns[num] + 'is the same pg_shard as ' + Cns[num + 1] + " , success")
+            else:
+                print(Cns[num] + 'is not the same pg_shard as ' + Cns[num + 1] + " , failure")
+        num += 1
+
+    num = 0
+    for i in ip:
+        conn=psycopg2.connect(database = 'postgres', user = user[num], host = i, port = port[num], password = pwd[num])
+        conn.autocommit = True
+        cur=conn.cursor()
+        execStmt1 = "select id, port, shard_id, svr_node_id, ro_weight, user_name,   hostaddr   , passwd from pg_shard_node"
+        cur.execute(execStmt1)
+        res = str(cur.fetchall())
+        Res = res1.replace(')',')\n')
+        cur.close()
+        conn.close()
+
+        with open("./ddl-diff/pg_shard_node%d" % (num),"w") as f:
+            f.write(Res)
+        num += 1
+
+    print("\n======== pg_shard ========\n")
+    num = 0
+    for i in Cns:
+        if i == Cns[-1]:
+            pass
+        else:
+            diff = filecmp.cmp("./ddl-diff/pg_shard_node%d" %(num), "./ddl-diff/pg_shard_node%d" %(num + 1))
+            if diff :
+                print(Cns[num] + 'is the same pg_shard_node as ' + Cns[num + 1] + " , success")
+            else:
+                print(Cns[num] + 'is not the same pg_shard_node as ' + Cns[num + 1] + " , failure")
+        num += 1
+
 
 def cheakAllCnsHasTpccRows():
     TpccTabName = ['history1', 'customer1', 'district1', 'item1', 'order_line1', 'orders1', 'stock1','warehouse1']
@@ -139,14 +248,22 @@ def cheakAllCnsHasTpccRows():
 
 
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'this script is use to test ddl replication!')
     parser.add_argument('--config', type=str, default = 'install.json', help = 'the configuration file of Kunlun_Cluster')
     args = parser.parse_args()
     File = args.config
     readJsonFile()
-    print('ip=[' + sip + ']\n' + 'port=' + sport + '\npwd= ' + pwd[0])
+    global Cns
+    Cns = []
+    num = 0
+    for i in ip:
+        Cn="%s:%s" % (i, str(port[num]))
+        Cns.append(Cn)
+        num += 1
+    #print('ip=[' + sip + ']\n' + 'port=' + sport + '\npwd= ' + pwd[0])
     #createDbAndConnect()
     #PrepareTpccAndRunTpcc5Second()
-    cheakAllCnsHasTpccRows()
+    #cheakAllCnsHasTpccRows()
     checkAllSystemTable()
