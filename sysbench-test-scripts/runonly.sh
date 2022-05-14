ba336d8bd1260846258126bc19942adaa6e4847c
charlesaaaaaaaa <i1925725098@outlook.com>
2022-05-14 11:38:59 +0800

    bak

--- sysbench-test-scripts/runonly.sh
+++ sysbench-test-scripts/runonly.sh
@@ -7,7 +7,13 @@ if [ $# -lt 7 ] ; then
 	exit 1
 fi
 
-
+for i in point_select delete insert read_only read_write write_only update_index update_non_index
+do
+        # create test result dir
+        if [ ! -d $i ] ; then
+                mkdir $i
+        fi
+done
 
 #for i in 9
 for i in 3 6 9 # run sysbench test 100 to 1000 threads
@@ -17,14 +23,12 @@ do
 	echo run threads ${li}
 	echo
 	./test.sh $1 $2 $3 $4 $li $5 $6 $7 
-	# like : ./loop_test.sh 192.168.0.134 38701 postgres abc  10        100000     120
-	#        ./loop_test.sh host          port  dbname   user table_num table_size test_time
 done
 
 bash ./result.sh 
 cp result result_before
 
-for n in `seq 1 5`
+for n in `seq 1 10`
 do
         for a in point_select write_only insert update_index update_non_index
         do
