2ca7e3eb09b1bc613ef7ef1230b48e662579143b
charlesaaaaaaaa <i1925725098@outlook.com>
2022-05-14 15:34:48 +0800

    bak

--- Python/check/clean-check.py
+++ Python/check/clean-check.py
@@ -10,9 +10,10 @@ def readJsonFile():
     cluster = fload['cluster']
     meta = cluster['meta']['nodes']
     data = cluster['data']
+    comp = cluster['comp']['nodes']
 
-    global metaport, metadir, dataport, datadir, metaip, dataip, allhost, ahost
-    metaport, metadir, dataport, datadir, metaip, dataip = [], [], [], [], [], []
+    global metaport, metadir, dataport, datadir, metaip, dataip, allhost, compip, ahost
+    metaport, metadir, dataport, datadir, metaip, dataip, compip = [], [], [], [], [], [], []
 
     for i in meta:
         Metaport = i['port']
@@ -31,10 +32,20 @@ def readJsonFile():
             dataport.append(Dataport)
             datadir.append(Datadir)
 
+    for i in comp:
+        Compip = i['ip']
+        compip.append(Compip)
+
     allhost=[]
     allhost.extend(metaip)
     allhost.extend(dataip)
+    allhost.extend(compip)
     ahost = list(set(allhost))
+    subprocess.run('mkdir -p check', shell = True)
+    subprocess.run('mkdir -p check/io', shell = True)
+    subprocess.run('mkdir -p check/net', shell = True)
+    subprocess.run('mkdir -p check/mem', shell = True)
+    subprocess.run('mkdir -p check/cpu', shell = True)
 
 def clean(ip, path):
     stmt = 'ssh ' + defuser + '@' + ip + " 'cd " + path + " && for i in `ls | sed \"$\"d | sed \"$\"d | sed \"$\"d`; do echo $i; rm $i; done'"
@@ -54,22 +65,22 @@ def check(ip):
     subprocess.run(stmt4, shell=True)
 
 def iostat(ip):
-    stmt1 = 'ssh ' + defuser + '@' + ip + " 'date && iostat' >> ./check/io-" + ip + '.sh'
+    stmt1 = 'ssh ' + defuser + '@' + ip + " 'date && iostat' >> ./check/io/io-" + ip + '.sh'
     print(stmt1)
     subprocess.run(stmt1, shell=True)
 
 def free(ip):
-    stmt2 = 'ssh ' + defuser + '@' + ip + " 'date && free' >> ./check/men-" + ip + '.sh'
+    stmt2 = 'ssh ' + defuser + '@' + ip + " 'date && free' >> ./check/mem/mem-" + ip + '.sh'
     print(stmt2)
     subprocess.run(stmt2, shell=True)
 
 def net(ip):
-    stmt3 = 'ssh ' + defuser + '@' + ip + " 'sar -n DEV 1 1 | grep -A 2 IFACE' >> ./check/net-" + ip + '.sh'
+    stmt3 = 'ssh ' + defuser + '@' + ip + " 'sar -n DEV 1 1 | grep -A 2 IFACE' >> ./check/net/net-" + ip + '.sh'
     print(stmt3)
     subprocess.run(stmt3, shell=True)
 
 def cpu(ip):
-    stmt4 = 'ssh ' + defuser + '@' + ip + " 'sar 1 1' >> ./check/cpu-" + ip + '.sh'
+    stmt4 = 'ssh ' + defuser + '@' + ip + " 'sar 1 1' >> ./check/cpu/cpu-" + ip + '.sh'
     print(stmt4)
     subprocess.run(stmt4, shell=True)
 
@@ -124,13 +135,29 @@ def all():
             num = 0
         sleep(10)
 
+def onlyCheck():
+    num = 0
+    while True:
+        for i in ahost:
+            checkThread(i)
+        num = num + 1
+        
+        sleep(10)
+
 if __name__ == '__main__':
     ps = argparse.ArgumentParser(description='check cluster healthy && clean each datanode binlog with 1 hour')
     ps.add_argument('--config', default='install.json', type=str, help='the configuration json file with Kunlun_cluster')
     ps.add_argument('--defuser', default='kunlun', type=str, help='default user with Kunlun_cluster')
+    ps.add_argument('--type', default='all', help='can be "all" or "onlycheck".')
     args=ps.parse_args()
     print(args)
     File = args.config
     defuser = args.defuser
+    type = args.type
     readJsonFile()
-    all()
+    if type == 'all':
+        all()
+    elif type == 'onlycheck':
+        onlyCheck()
+
+
