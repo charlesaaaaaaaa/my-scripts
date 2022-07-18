978e440b557987b02a2dd092b52e41fe8bf6796a
charlesaaaaaaaa <i1945725098@outlook.com>
2022-07-18 18:59:26 +0800

    bak

--- Python/sysbenchtest/pid.sh
+++ Python/sysbenchtest/pid.sh
@@ -3,7 +3,6 @@ pids=`ps -fe | grep sysbench | grep $1 | grep $2 | awk '{print $2}' | wc -l`
 if [ "$pids" -ne 0 ]
 then
 	ps -fe | grep sysbench | grep $1 | grep $2 | awk '{print $2}' > pid.log
-	ps -fe | grep sysbench | grep $1 | grep $2 | awk '{print $2}'
 else
 	rm -rf pid.log
 fi
