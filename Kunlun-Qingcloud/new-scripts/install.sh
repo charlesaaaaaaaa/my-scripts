2f123465b3d0d316141646b1b92cec9a196b3314
charlesaaaaaaaa <i1945725098@outlook.com>
2022-08-15 15:59:34 +0800

    bak

--- Kunlun-Qingcloud/new-scripts/install.sh
+++ Kunlun-Qingcloud/new-scripts/install.sh
@@ -95,7 +95,7 @@ then
 	python2 install-mysql.py --config=/home/kunlun/conf/mysql_meta.json --target_node_index=0 --cluster_id=meta --shard_id=meta --server_id=1 --ha_mode=mgr
 	cd /home/kunlun/base/kunlun-cluster-manager-1.0.1/bin
 	bash start_cluster_mgr.sh </dev/null >& start.log & 
-	cd /home/kunlun/base/kunlun-cluster-node--1.0.1/bin
+	cd /home/kunlun/base/kunlun-node-manager--1.0.1/bin
 	bash start_node_mgr.sh </dev/null >& start.log &
 	sleep 15
 	cd $HOME
@@ -137,25 +137,25 @@ then
 	python2 install-mysql.py --config=/home/kunlun/conf/mysql_meta.json --target_node_index=$tni --cluster_id=meta --shard_id=meta --server_id=$serid --ha_mode=mgr
 	cd /home/kunlun/base/kunlun-cluster-manager-1.0.1/bin
         bash start_cluster_mgr.sh </dev/null >& start.log &
-        cd /home/kunlun/base/kunlun-cluster-node--1.0.1/bin
+        cd /home/kunlun/base/kunlun-node-manager-1.0.1/bin
         bash start_node_mgr.sh </dev/null >& start.log &
 
 
 elif [[ "$selfRole" == "computer_node" ]]
 then
 	/bin/bash change_conf.sh node $selfIp "$clusterMetaSeeds"
-	cd /home/kunlun/base/kunlun-cluster-node--1.0.1/bin
+	cd /home/kunlun/base/kunlun-node-manager-1.0.1/bin
         bash start_node_mgr.sh </dev/null >& start.log &
 
 elif [[ "$selfRole" == "data_node" ]]
 then
 	/bin/bash change_conf.sh node $selfIp "$clusterMetaSeeds"
-        cd /home/kunlun/base/kunlun-cluster-node--1.0.1/bin
+        cd /home/kunlun/base/kunlun-node-manager-1.0.1/bin
         bash start_node_mgr.sh </dev/null >& start.log &
 
 elif [[ "$selfRole" == "data_node-replice" ]]
 then
         /bin/bash change_conf.sh node $selfIp "$clusterMetaSeeds"
-        cd /home/kunlun/base/kunlun-cluster-node--1.0.1/bin
+        cd /home/kunlun/base/kunlun-node-manager-1.0.1/bin
         bash start_node_mgr.sh </dev/null >& start.log &
 fi
