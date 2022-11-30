64c5fdc584b5d5d00aa88eb6c009096bd8fdcd7a
charlesaaaaaaaa <i1945725098@outlook.com>
2022-11-30 17:08:44 +0800

    bak

--- Python/sysbenchtest/prepare.sh
+++ Python/sysbenchtest/prepare.sh
@@ -22,12 +22,12 @@ sysbench oltp_point_select        \
 
 date
 
-for i in point_select point_select_k insert write_only read_only read_only_k read_write read_write_k update_index update_non_index
-do
+#for i in point_select point_select_k insert write_only read_only read_only_k read_write read_write_k update_index update_non_index
+#do
 	# create test result dir
-	if [ ! -d $i ] ; then
-		mkdir $i 
-	fi
-done
+#	if [ ! -d $i ] ; then
+#		mkdir $i 
+#	fi
+#done
 
 #sysbench oltp_delete --tables=10 --table-size=10000000 --db-driver=pgsql --pgsql-host=192.168.0.132 --pgsql-port=8881 --pgsql-user=abc --pgsql-db=postgres --pgsql-password=abc cleanup
