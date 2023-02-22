da9894527be7f196ca1f130cead4060a5eb68db8
charlesaaaaaaaa <i1945725098@outlook.com>
2023-02-22 18:53:53 +0800

    bak

--- Python/xinTongYuan/cluster_manager/deploy_install.py
+++ Python/xinTongYuan/cluster_manager/deploy_install.py
@@ -8,9 +8,16 @@ import argparse
 start_time = time.time()
 def deploy_cluster():
     subprocess.run("rm -rf *gz* && rm -rf cloudnative", shell=True)
+    start_cloud_time = time.time()
+    print('start clone cloudnative ...')
     subprocess.run("git clone https://gitee.com/zettadb/cloudnative.git", shell=True)
+    end_cloud_time = time.time()
+    final_cloud_time = end_cloud_time - start_cloud_time 
+    print('clone cloudnative: %.2f s' % (final_cloud_time))
     thirdPart_downList = ['hadoop-3.3.1.tar.gz', 'jdk-8u131-linux-x64.tar.gz', 'mysql-connector-python-2.1.3.tar.gz', 'prometheus.tgz', 'haproxy-2.5.0-bin.tar.gz', 'efk/filebeat-7.10.1-linux-x86_64.tar.gz']
     kunlun_downList = ['kunlun-cluster-manager-1.1.1.tgz', 'kunlun-node-manager-1.1.1.tgz', 'kunlun-server-1.1.1.tgz', 'kunlun-storage-1.1.1.tgz', 'kunlun-proxysql-1.1.1.tgz']
+    print('download component ...')
+    start_downl_time = time.time()
     for i in thirdPart_downList:
         Url = "http://zettatech.tpddns.cn:14000/thirdparty/%s" % (str(i))
         print("\ndownloading %s ..."% (i))
@@ -19,12 +26,20 @@ def deploy_cluster():
         Url = "http://zettatech.tpddns.cn:14000/dailybuilds/enterprise/%s" % (str(i))
         print("\ndownloading %s ..." % (i))
         wget.download(Url)
+    end_downl_time = time.time()
+    final_downl_time = end_downl_time - start_downl_time
+    print('download component : %.2f s' % (final_downl_time))
     subprocess.run("mv *gz* cloudnative/cluster/clustermgr", shell=True)
     subprocess.run("cp %s cloudnative/cluster" % (Deploy), shell=True)
+    start_deploy_time = time.time()
+    print('start deploy KunlunBase ...')
     deploy_command = "cd cloudnative/cluster && python2 setup_cluster_manager.py --config %s --action install --defuser %s --product_version 1.1.1" % (Deploy, User)
     print(deploy_command)
     subprocess.run(deploy_command, shell=True)
     subprocess.run("cd cloudnative/cluster && bash -e clustermgr/install.sh", shell=True)
+    end_deploy_time = time.time()
+    final_deploy_time = end_deploy_time - start_deploy_time
+    print('deploy KunlunBase : %.2f s' % (final_deploy_time))
 
 if __name__ == '__main__':
     ps = argparse.ArgumentParser(description='deploy&install KunlunBase cluster')
@@ -41,4 +56,4 @@ if __name__ == '__main__':
 
     end_time = time.time()
     spend_time = end_time - start_time
-    print("本次安装部署花费了：%s 秒" % (spend_time))
+    print("本次安装部署花费了：%.2f 秒" % (spend_time))
