451d6be9debc262bee65162bee737df8ae67f70c
charlesaaaaaaaa <i1945725098@outlook.com>
2023-03-17 17:49:48 +0800

    bak

--- Python/xinTongYuan/cluster_manager/deploy_install.py
+++ Python/xinTongYuan/cluster_manager/deploy_install.py
@@ -15,15 +15,15 @@ def deploy_cluster():
     final_cloud_time = end_cloud_time - start_cloud_time 
     print('clone cloudnative: %.2f s' % (final_cloud_time))
     thirdPart_downList = ['hadoop-3.3.1.tar.gz', 'jdk-8u131-linux-x64.tar.gz', 'mysql-connector-python-2.1.3.tar.gz', 'prometheus.tgz', 'haproxy-2.5.0-bin.tar.gz', 'efk/filebeat-7.10.1-linux-x86_64.tar.gz']
-    kunlun_downList = ['kunlun-cluster-manager-1.1.1.tgz', 'kunlun-node-manager-1.1.1.tgz', 'kunlun-server-1.1.1.tgz', 'kunlun-storage-1.1.1.tgz', 'kunlun-proxysql-1.1.1.tgz']
+    kunlun_downList = ['kunlun-cluster-manager-%s.tgz' % (ProductVersion), 'kunlun-node-manager-%s.tgz' % (ProductVersion), 'kunlun-server-%s.tgz' % (ProductVersion), 'kunlun-storage-%s.tgz' % (ProductVersion), 'kunlun-proxysql-%s.tgz' % (ProductVersion)]
     print('download component ...')
     start_downl_time = time.time()
     for i in thirdPart_downList:
-        Url = "%s%s" % (Link, str(i))
+        Url = "%s/thirdparty/%s" % (Link, str(i))
         print("\ndownloading %s ..."% (i))
         wget.download(Url)
     for i in kunlun_downList:
-        Url = "%s%s" % (Link, str(i))
+        Url = "%s/dailybuilds/enterprise/%s" % (Link, str(i))
         print("\ndownloading %s ..." % (Url))
         wget.download(Url)
     end_downl_time = time.time()
@@ -33,7 +33,7 @@ def deploy_cluster():
     subprocess.run("cp %s cloudnative/cluster" % (Deploy), shell=True)
     start_deploy_time = time.time()
     print('start deploy KunlunBase ...')
-    deploy_command = "cd cloudnative/cluster && python2 setup_cluster_manager.py --config %s --action install --defuser %s --product_version 1.1.1" % (Deploy, User)
+    deploy_command = "cd cloudnative/cluster && python2 setup_cluster_manager.py --config %s --action install --defuser %s --product_version %s" % (Deploy, User, ProductVersion)
     print(deploy_command)
     subprocess.run(deploy_command, shell=True)
     subprocess.run("cd cloudnative/cluster && bash -e clustermgr/install.sh", shell=True)
@@ -47,8 +47,10 @@ if __name__ == '__main__':
     ps.add_argument("--user", default='kunlun', type=str, help='KunlunBase user')
     ps.add_argument("--link", default='http://zettatech.tpddns.cn:14000/dailybuilds/enterprise/', type=str, help='Kunlun-component download link')
     ps.add_argument("--version", default='1.1', type=str, help='cloudnative version')
+    ps.add_argument("--product_version", default='1.1.2', type=str, help='KunlunBase version')
     args = ps.parse_args()
     Version = args.version
+    ProductVersion = args.product_version
     User = args.user
     Deploy = args.depoly
     Link = args.link
