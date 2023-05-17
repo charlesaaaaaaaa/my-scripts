bf4e9572a09c7078c5bbec7faa5173abaee12de6
charlesaaaaaaaa <i1945725098@outlook.com>
2023-05-17 17:08:13 +0800

    bak

--- Python/xinTongYuan/explain-test.py
+++ Python/xinTongYuan/explain-test.py
@@ -1,12 +1,12 @@
-import psycopg2
-import pymysql
 import random
+import pymysql
 import argparse
+import psycopg2
 from time import sleep
 
-def connect_pg(host, port, user, pwd):
+def connect_pg(host, port, user, pwd, db):
     global cur, conn
-    conn = psycopg2.connect(host=host, port=port, user=user, password=pwd, database='postgres')
+    conn = psycopg2.connect(host=host, port=port, user=user, password=pwd, database=db)
     autocommit = True
     cur = conn.cursor()
 
@@ -24,9 +24,9 @@ def close_pg(fet):
     conn.close()
     return result
 
-def connect_mysql(host, port, sql):
+def connect_mysql(host, port, db, sql):
     global result
-    conn = pymysql.connect(host=host, port=port, user='pgx', passwd='pgx_pwd', database='postgres_$$_explains')
+    conn = pymysql.connect(host=host, port=port, user='pgx', passwd='pgx_pwd', database=db)
     cur = conn.cursor()
     cur.execute(sql)
     result = cur.fetchall()
@@ -35,82 +35,89 @@ def connect_mysql(host, port, sql):
     conn.close()
     return result
 
-def t2str(value):
-    global values
-    values = str(value)
-    values = values.replace('[', '')
-    values = values.replace(']', '')
-    values = values.replace(',', '')
-    values = values.replace('(', '')
-    values = values.replace(')', '')
-    values = values.replace(' ', '')
-    values = values.replace('\'', '')
-    return values
-
-
 def test():
-    print("1）	准备一张分片表，并插入部分数据；")
-    connect_pg(Host, Port, User, Pwd)
-    cur.execute("drop schema  if exists explains CASCADE")
-    cur.execute("create schema explains")
-    print("create schema explains")
-    close_pg('passtable item (id int, name text')
-    connect_pg(Host, Port, User, Pwd)
-    cur.execute("create table explains.item (id int, name text)")
-    print("create table explains.item (id int, name text")
-    close_pg('pass')
-    print("loading 1000 row data...")
-    connect_pg(Host, Port, User, Pwd)
-    for i in range(1,1001):
-        ranNum = random.randint(15,20)
+    print("1）  准备一张分片表，并插入部分数据；")
+    connect_pg(Host, Port, User, Pwd, 'postgres')
+    cur.execute("drop database if exists explaindb")
+    cur.execute("create database explaindb")
+    close_pg('n')
+    connect_pg(Host, Port, User, Pwd, 'explaindb')
+    sql="create table test(a int, b text) partition by hash(a)"
+    cur.execute(sql)
+    print(sql)
+    for i in range(6):
+        sql = "create table test_%s partition of test for values with (MODULUS 6, REMAINDER %s)" % (i, i)
+        cur.execute(sql)
+        print(sql)
+    close_pg('n')
+    print('loading data')
+    connect_pg(Host, Port, User, Pwd, 'explaindb')
+    for i in range(1, 1001):
         ranStr = random.choice(['liangzai','is','llc','charles','trn','jgh','fhrq','fhxe','gewb'])
-        cur.execute("insert into explains.item values(%s, '%s')" % (str(i), ranStr))
-    close_pg('pass')
+        ranInt = random.randint(99, 1000)
+        sql = "insert into test values(%s, '%s%s')" % (i, ranStr, ranInt)
+        cur.execute(sql)
+    close_pg('n')
+    print("\n2）        查询一条跨库语句的执行计划；")
+    ranNum = random.randint(1, 1001)
+    connsql = 'create extension if not exists dblink;'
+    connsql1 = "select dblink_connect('conn1', concat('hostaddr=%s port=%s dbname=%s user=%s password=%s', inet_server_port()));" % (Host, Port,'explaindb', User, Pwd)
+    selectsql = "select * from dblink('conn1', 'explain select * from test where a = %s') as t1(a text);" % (ranNum)
+    sql = "%s %s %s" % (connsql, connsql1, selectsql)
+    connect_pg(Host, Port, User, Pwd, 'postgres')
+    cur.execute(connsql)
+    print(connsql)
+    print(connsql1)
+    cur.execute(connsql1)
+    print(selectsql)
+    cur.execute(selectsql)
+    res = close_pg('all')[2][0]
+    print(res)
+    shardNum = res.split(':')[1][1]
+    tableName = res.split('.')[3][1:7]
+    print("================\n该条sql影响到 shard_%s, 表名为：%s\n================\n" % (shardNum, tableName))
+
+    print("3）  执行这条跨库语句。")
+    selectsql = "select * from dblink('conn1', 'select * from test where a = %s') as t1(a int, b text);" % (ranNum)
+    connect_pg(Host, Port, User, Pwd, 'postgres')
+    cur.execute(connsql)
+    cur.execute(connsql1)
+    print(selectsql)
+    cur.execute(selectsql)
+    pgres = close_pg('one')
+    print(pgres)
+
+    #找到元数据主
+    hostaddr = "select hostaddr from pg_cluster_meta_nodes where is_master = 't'"
+    port = "select port from pg_cluster_meta_nodes where is_master = 't'"
+    clusterid = "select cluster_id from pg_cluster_meta_nodes where is_master = 't'"
+    connect_pg(Host, Port, User, Pwd, 'postgres')
+    cur.execute(hostaddr)
+    metahostaddr = close_pg('one')[0]
+    connect_pg(Host, Port, User, Pwd, 'postgres')
+    cur.execute(port)
+    metaport = close_pg('one')[0]
+    connect_pg(Host, Port, User, Pwd, 'postgres')
+    cur.execute(clusterid)
+    clusterid = close_pg('one')[0]
+    #去元数据上找对应shard的主存储节点
+    dbhostaddr = "select hostaddr from shard_nodes where db_cluster_id = %s and shard_id = %s and member_state = 'source';" % (clusterid, shardNum)
+    dbport = "select port from shard_nodes where db_cluster_id = %s and shard_id = %s and member_state = 'source';" % (clusterid, shardNum)
+    storage_host = connect_mysql(metahostaddr, metaport, 'kunlun_metadata_db', dbhostaddr)[0][0]
+    sharage_port = connect_mysql(metahostaddr, metaport, 'kunlun_metadata_db', dbport)[0][0]
+    print("\n现在去 shard_%s 主节点 %s:%s 的 %s表 检查数据是否存在" % (shardNum, storage_host, sharage_port, tableName))
     
-    print("\n2）	查询一条跨库语句的执行计划；")
-    connect_pg(Host, Port, User, Pwd)
-    ranNum = random.randint(1,1001)
-    sql = "explain select * from explains.item where id = %s" % (str(ranNum))
+    #查找对应数据
+    sql = "select * from %s where a = %s" % (tableName, ranNum)
     print(sql)
-    cur.execute(sql)
-    result = close_pg("all")
-    print(result)
-    seconds = str(result[1:8])
-    shardNum = seconds.split(':')[1].split('\\')[0]
-    shardNum = t2str(shardNum)
-    print("================\n该条sql影响到 shard_%s\n================\n" % (shardNum))
-
-    print("3）	执行这条跨库语句。")
-    connect_pg(Host, Port, User, Pwd)
-    sql = "select * from explains.item where id = %s" % (str(ranNum))
-    print(sql)
-    cur.execute(sql)
-    result = close_pg("all")
-    print(result)
-
-    print("\n现在去shard_%s验证" % (shardNum))
-    shardHostSql = 'select hostaddr from pg_shard_node where shard_id = %s' % (shardNum)
-    shardPortSql = 'select port from pg_shard_node where shard_id = %s' % (shardNum)
-    sqlMy = 'select * from item where id = %s' % (str(ranNum))
-    connect_pg(Host, Port, User, Pwd)
-    cur.execute(shardHostSql)
-    shard_host = cur.fetchone()
-    cur.execute(shardPortSql)
-    shard_port = cur.fetchone()
-    close_pg('pass')
-    shard_host = t2str(shard_host)
-    shard_port = t2str(shard_port)
-    print("%s:%s '%s'" % (shard_host, shard_port, sqlMy))
-    resultMy = connect_mysql(shard_host, int(shard_port), sqlMy)
-    print(resultMy)
-    result = t2str(result)
-    resultMy = t2str(resultMy)
-
-    if result == resultMy:
-        print("数据相同，验证通过")
-    else :
-        print("数据不相同，验证失败")
-
+    myres = connect_mysql(storage_host, sharage_port, 'explaindb_$$_public', sql)[0]
+    print(myres)
+    if pgres == myres:
+        print('\n== 数据相同，验证通过')
+    else:
+        print('\n== 数据不相同，验证失败')
+        exit(1)
+    
 if __name__ == '__main__':
     ps = argparse.ArgumentParser(description="Kunlun explain test")
     ps.add_argument("--host", type=str, help='The host of KunlunBase cluster')
