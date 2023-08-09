696a1a7d47f5457beb59d29c33ae3c2009fd5f0d
charlesaaaaaaaa <i1945725098@outlook.com>
2023-08-09 14:36:19 +0800

    bak

--- Python/cdc_test/cdc_test.py
+++ Python/cdc_test/cdc_test.py
@@ -4,20 +4,20 @@ from base.other import copyconfig
 def mysqlToKlustron():
     from base import test
     from base.other.otherOpt import restartCdcCluster
-    restartCdcCluster()
     test.mysqlToKunlun().regular_test()
     test.mysqlToKunlun().killCdcMasterWhenCdcIsRunning()
     test.mysqlToKunlun().killSourceMysqlWhenCdcIsRunning()
     test.mysqlToKunlun().killTargeKlustronWherCdcIsRunning()
+    restartCdcCluster()
 
 def kunlunToMysql():
     from base.test import kunlunToMysql
     from base.other.otherOpt import restartCdcCluster
-    restartCdcCluster()
     kunlunToMysql().regular_test()
     kunlunToMysql().killCdcMasterWhenCdcIsRunning()
     kunlunToMysql().killTargetMysqlWhenCdcIsRunning()
     kunlunToMysql().killSourceKunlunWhenCdcIsRunning()
+    restartCdcCluster()
 
 if __name__ == '__main__':
     copyconfig.Mode('regular')
