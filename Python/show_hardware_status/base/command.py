a3332a9a3304762a5433e9800c4f14e23aebbe1f
charlesaaaaaaaa <i1945725098@outlook.com>
2023-09-27 11:47:57 +0800

    bak

--- Python/show_hardware_status/base/command.py
+++ Python/show_hardware_status/base/command.py
@@ -5,24 +5,24 @@ class comm():
 
     def cpu(self):
         comm = self.half_comm
-        cpu_c = "'mpstat' >> ./result/%s/cpu 2>&1" % self.ip
+        cpu_c = "'date;mpstat' >> ./result/%s/cpu 2>&1" % self.ip
         comm += cpu_c
         return comm
 
     def mem(self):
         comm = self.half_comm
-        mem_c = "'free -h' >> ./result/%s/mem 2>&1" % self.ip
+        mem_c = "'date;free -h' >> ./result/%s/mem 2>&1" % self.ip
         comm += mem_c
         return comm
 
     def net(self):
         comm = self.half_comm
-        net_c = "'sudo iftop -ts 1 -o 2s' >> ./result/%s/net 2>&1" % self.ip
+        net_c = "'date; sudo iftop -ts 1 -o 2s' >> ./result/%s/net 2>&1" % self.ip
         comm += net_c
         return comm
 
     def disk(self):
         comm = self.half_comm
-        disk_c = "'iostat -dxh' >> ./result/%s/disk 2>&1" % self.ip
+        disk_c = "'date; iostat -dxh' >> ./result/%s/disk 2>&1" % self.ip
         comm += disk_c
         return comm
--- Python/show_hardware_status/show_hardware_status.py
+++ Python/show_hardware_status/show_hardware_status.py
@@ -1,3 +1,5 @@
+import multiprocessing
+import threading
 from threading import Thread
 from base.command import *
 import subprocess
@@ -11,25 +13,51 @@ def show():
     user = conf['user']
     interval = int(conf['interval'])
 
-    def signal_thread(ip):
+    def show_process(ip):
         com = comm(ip, user)
-        for i in 'cpu', 'mem', 'net', 'disk':
-            mk_c = 'mkdir -p result/%s/; touch result/%s/%s' % (ip, i)
-            subprocess.run(mk_c, shell=True)
-        while True:
-            subprocess.run(com.cpu(), shell=True)
-            subprocess.run(com.mem(), shell=True)
-            subprocess.run(com.net(), shell=True)
-            subprocess.run(com.disk(), shell=True)
-            sleep(interval)
+        command = 'mkdir -p result/%s' % (ip)
+        subprocess.run(command, shell=True)
+        for i in 'cpu', 'net', 'mem', 'disk':
+            command = 'touch result/%s/%s' % (ip, i)
+            subprocess.run(command, shell=True)
+        def show_cpu():
+            while True:
+                subprocess.run(com.cpu(), shell=True)
+                sleep(interval)
+        def show_mem():
+            while True:
+                subprocess.run(com.mem(), shell=True)
+                sleep(interval)
+        def show_net():
+            while True:
+                subprocess.run(com.net(), shell=True)
+                sleep(interval)
+        def show_disk():
+            while True:
+                subprocess.run(com.disk(), shell=True)
+                sleep(interval)
+        collect_cpu = threading.Thread(target=show_cpu)
+        collect_mem = threading.Thread(target=show_mem)
+        collect_net = threading.Thread(target=show_net)
+        collect_disk = threading.Thread(target=show_disk)
+        collect_cpu.start()
+        collect_mem.start()
+        collect_net.start()
+        collect_disk.start()
+        collect_disk.join()
+        collect_net.join()
+        collect_mem.join()
+        collect_cpu.join()
 
     l = []
     rm_c = 'rm -rf ./result/*'
     subprocess.run(rm_c, shell=True)
     for ip in host:
-        p = Thread(target=signal_thread, args=[ip])
+        p = multiprocessing.Process(target=show_process, args=(ip,))
         l.append(p)
         p.start()
+    for li in l:
+        li.join()
 
 if __name__ == '__main__':
     show()
